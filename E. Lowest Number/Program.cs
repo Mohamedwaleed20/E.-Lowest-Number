76cd4cdb7d4f9c02d4788e271ad7ad652689d292
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-16 12:18:55 +0300

    Add project files.

---
+++ E. Lowest Number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int min = int.MaxValue;
+        int pos = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int num = Convert.ToInt32(nums[i]);
+            if (num < min)
+            {
+                min = num;
+                pos = i + 1;
+            }
+        }
+
+        Console.WriteLine(min + " " + pos);
+    }
+}
